24be4880ce4c5603d675ea008423822639680114
Simplex <kontakt.simplex@pm.me>
2025-01-05 21:02:01 +0100

    fix: 🐛 About Command didnt work correctly

--- ext/commands.py
+++ ext/commands.py
@@ -84,9 +84,7 @@ class Commands(commands.Cog):
         await interaction.response.send_message(embed=embed)
         
     @app_commands.command(name="about", description="About me")
-    async def about(self, interaction: discord.Interaction):
-        DEV = self.bot.get_user(579111799794958377)
-        
+    async def about(self, interaction: discord.Interaction):        
         embed = discord.Embed(
             title=f"About {self.bot.user.name}",
             color=discord.Color.blue(),
@@ -98,18 +96,19 @@ class Commands(commands.Cog):
             name="<:info:1226139199351296051> General Information",
             value=f"> - `Bot Name:` *{self.bot.user.name}#{self.bot.user.discriminator}*" + 
                 f"\n> - `Bot ID:` *{self.bot.user.id}*" +
-                f"\n> - `Developer:` *{DEV.name}*" + 
-                f"\n> - `Developer ID:` *{DEV.id}*",
+                "\n> - `Developer:` *.smplx*" + 
+                "\n> - `Developer ID:` *579111799794958377*",
             inline=False
         )
         
         embed.add_field(
             name="<:info:1226139199351296051> Links",
-            value="> - [GitHub](https://github.com/SimplexDE/Starchaser)",
+            value="> - [GitHub](https://github.com/SimplexDE/Starchaser)" \
+                "\n> - [Invite me](https://discord.com/oauth2/authorize?client_id=881186799538540565&permissions=322560&integration_type=0&scope=applications.commands+bot)",
             inline=False
         )
         
-        embed.set_author(name="🧑‍💻 Simplex", url=f"https://discord.com/users/{DEV.id}", icon_url=DEV.avatar.url)
+        embed.set_author(name="🧑‍💻 Simplex", url="https://discord.com/users/881186799538540565")
         embed.set_thumbnail(url=self.bot.user.avatar.url)
         embed.set_footer(text="Licsensed under MIT", icon_url=self.bot.user.avatar.url)
         
